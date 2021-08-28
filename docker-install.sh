8b4da2ff9a380c8c78ac679d3a08794decebf53d
shivapoudyal <shivpodi04@gmail.com>
2021-08-28 19:01:02 +0530

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose version
