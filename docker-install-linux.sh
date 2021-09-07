e45ea34758f3a3d14e44c7f9221265553c662da0
shivapoudyal <shivpodi04@gmail.com>
2021-09-07 11:40:33 +0530

    Update docker-install-linux.sh

--- docker-install-linux.sh
+++ docker-install-linux.sh
@@ -5,4 +5,5 @@ sudo chkconfig docker on
 sudo yum install -y git
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +xr /home/ec2-user/docker/.docker-compose.yml.swp
 docker-compose version
