8c278251afac13f52e61a8924a62a8bc82938a55
shivapoudyal <shivpodi04@gmail.com>
2021-09-26 13:13:39 +0530

    Create network-in-docker-compose.py

---
+++ network-in-docker-compose.py
@@ -0,0 +1,30 @@
+version: '3.1'
+services:
+    db:
+        image: mysql
+        restart: always
+        environment:
+          MYSQL_ROOT_PASSWORD: root
+          MYSQL_DATABASE: medical_system
+        container_name: mysql
+        volumes: 
+          - ./mysql_data:/var/lib/mysql/
+        ports:
+          - "3308:3306"
+        networks:
+          - custom_network
+    phpmyadmin:
+        image: phpmyadmin/phpmyadmin:latest
+        restart: always
+        environment:
+          PMA_HOST: db
+          PMA_USER: root
+          PMA_PASSWORD: root
+        ports:
+          - "8080:80"
+        networks:
+          - custom_network 
+networks:
+  custom_network:
+    external:
+      name: codeigniter-simple-project_php-nginx 
